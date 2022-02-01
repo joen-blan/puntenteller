1030842611838928e099bb11c01a9c283a847815
joen-blan <98757172+joen-blan@users.noreply.github.com>
2022-02-01 10:02:30 +0100

    Delete footer.php

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-  <div class="content has-text-centered">
-    <p>
-      <strong>Puntenteller</strong> voor <a href="https://www.gbsdestelbergen.be/">Gemeentelijke basisschool Destelbergen</a>.
-    </p>
-  </div>
-</footer>
